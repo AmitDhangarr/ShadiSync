9d1f890b5ae565fb877b6d0357521359e9ec7a14
Amit Dhangar <amitdhangar531@gmail.com>
2026-06-18 17:58:42 +0530

    cors and rate-limiting has setup

--- server.js
+++ server.js
@@ -4,29 +4,55 @@ import { DBconnection } from "./database/db.js";
 import userRoute from "./routes/user.route.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import AuthMiddleware from "./middlewares/auth.middleware.js";
 import UserAuthorisation from "./middlewares/authorisation.middleware.js";
 import hostRoute from "./routes/host/host.route.js";
-import GuestRoute from './routes/guest/guest.route.js';
+import GuestRoute from "./routes/guest/guest.route.js";
+import rateLimit from "express-rate-limit";
+// cors configuration
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-API-KEY"],
+};
 
+// rate limiting config
+
+const ratelimitConfig = {
+  windowMs: 5 * 60 * 1000,
+  max: (req, res) => {
+    if (req.cookies?.token || req.headers["authorization"]) return 20;
+    return 5;
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    status: 429,
+    message: "Too many requests. Limit is 5-20 per minute.",
+  },
+};
 
 class StartServer {
   app = express();
   port = null;
+  apirateLimit = null;
 
   constructor() {
     this.setConfiguration();
+    this.setMongoConnection();
     this.setRoutes();
     this.start();
-    this.setMongoConnection();
   }
   setConfiguration() {
     dotenv.config({ path: "./.env" });
     this.port = process.env.PORT;
     this.connection = process.env.MONGODBURL;
+    this.app.use(cors(corsOptions));
+    this.app.use(cookieParser());
+    this.apirateLimit = rateLimit(ratelimitConfig);
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.use(cookieParser());
   }
   start() {
     this.app.listen(this.port, () =>
@@ -34,9 +60,15 @@ class StartServer {
     );
   }
   setRoutes() {
-    this.app.use("/",userRoute);
-    this.app.use("/host",AuthMiddleware.AuthUser,UserAuthorisation.checkRole,hostRoute);
-    this.app.use("/guest",AuthMiddleware.AuthUser,GuestRoute);
+    this.app.use("/api/v1/", this.apirateLimit);
+    this.app.use("/api/v1/auth", userRoute);
+    this.app.use(
+      "/api/v1/host",
+      AuthMiddleware.AuthUser,
+      UserAuthorisation.checkRole,
+      hostRoute,
+    );
+    this.app.use("/api/v1/guest", AuthMiddleware.AuthUser, GuestRoute);
   }
   setMongoConnection() {
     const connection = DBconnection(this.connection);
